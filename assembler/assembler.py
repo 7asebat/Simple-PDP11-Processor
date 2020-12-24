906e01e88d31988c4ab677e25e84897584f260ec
Abdelrahman Farid <abdelrahman.farid99@eng-st.cu.edu.eg>
2020-12-24 12:00:46 +0200

    Initial commit, added Nasser and Hesham's fully functional Assembler.

---
+++ assembler/assembler.py
@@ -0,0 +1,93 @@
+from utility import *
+
+if len(sys.argv) != 2:
+    print('Invalid arguments')
+    sys.exit(1)
+
+with open(sys.argv[1]) as f:
+    HLTCalled = False
+    curr_addr = 0
+    for (index, line) in enumerate(f):
+        # removing extra spaces
+        line = line.rstrip().strip().upper()
+
+        # Check: if empty line ignore
+        if len(line) <= 1 or line.startswith(';'):  # NEED TO BE EDITED
+            continue
+
+        # Update current address
+        if '.=' in line:
+            curr_addr = int(line.split('.=')[-1].strip())
+            continue
+
+        # Defining variables
+        if line.startswith('DEFINE'):
+            curr_addr = define_variable(line, variables, Memory, curr_addr)
+            continue
+
+        # Defining labels
+        if ':' in line:
+            define_label(line, labels, curr_addr)
+
+        if HLTCalled:
+            continue
+
+        # Line sanity
+        instruction = sanitize_line(line)
+        if not instruction:
+            continue
+        print(instruction)
+
+        # Constructing bit string
+        bitString = ''
+
+        # Load instruction opcode
+        if not instruction[0] in instruction_opcodes:
+            print('Error in parsing instruction: syntax error in line', index + 1)
+            sys.exit(1)
+
+        # Load instruction opcode to the bit string
+        bitString += instruction_opcodes[instruction[0]]
+
+        if instruction[0] in two_op_instructions:
+            curr_addr = process_two_op(instruction, bitString, Memory, curr_addr)
+
+        elif instruction[0] in one_op_instructions:
+            curr_addr = process_one_op(instruction, bitString, Memory, curr_addr)
+
+        elif instruction[0] in branch_instructions:
+            curr_addr = process_branch(instruction, bitString, Memory, curr_addr)
+
+        elif instruction[0] in no_op_instructions:
+            curr_addr = process_no_op(instruction, bitString, Memory, curr_addr)
+
+        elif instruction[0] in jsr_instructions:
+            curr_addr = process_jsr(instruction, bitString, Memory, curr_addr)
+
+        else:
+            print('Invalid instruction: syntax error in line', index + 1)
+            sys.exit(1)
+
+        if instruction[0] == 'HLT':
+            HLTCalled = True
+
+        curr_addr += 1
+
+# Post-processing to update labels offset
+for (key, value) in Memory.items():
+    sanitize_label(key, value, labels, Memory)
+    sanitize_variable(key, value, variables, Memory)
+
+write_memory_to_file('memory.bin')
+
+print('\nVariables:')
+for k, v in variables.items():
+    print(k, v)
+
+print('\nLabels:')
+for k, v in labels.items():
+    print(k, v)
+
+print('\nMemory:')
+for k, v in Memory.items():
+    print(v)
---
+++ assembler/utility.py
@@ -0,0 +1,330 @@
+import sys
+import re
+
+Memory = {}
+
+pre_one_op = '1001'
+pre_branch = '11'
+pre_no_op = '1010'
+pre_jump = '1011'
+instruction_opcodes = {
+    'MOV':       '0000',
+    'ADD':       '0001',
+    'ADC':       '0010',
+    'SUB':       '0011',
+    'SBC':       '0100',
+    'AND':       '0101',
+    'OR':        '0110',
+    'XOR':       '0111',
+    'CMP':       '1000',
+    'INC':       pre_one_op + '0000',
+    'DEC':       pre_one_op + '0001',
+    'CLR':       pre_one_op + '0010',
+    'INV':       pre_one_op + '0011',
+    'LSR':       pre_one_op + '0100',
+    'ROR':       pre_one_op + '0101',
+    'ASR':       pre_one_op + '0110',
+    'LSL':       pre_one_op + '0111',
+    'ROL':       pre_one_op + '1000',
+    'BR':        pre_branch + '000',
+    'BEQ':       pre_branch + '001',
+    'BNE':       pre_branch + '010',
+    'BLO':       pre_branch + '011',
+    'BLS':       pre_branch + '100',
+    'BHI':       pre_branch + '101',
+    'BHS':       pre_branch + '110',
+    'HLT':       pre_no_op  + '0000',
+    'NOP':       pre_no_op  + '0001',
+    'RESET':     pre_no_op  + '0010',
+    'JSR':       pre_jump   + '0000',
+    'RTS':       pre_jump   + '0001',
+    'INTERRUPT': pre_jump   + '0010',
+    'IRET':      pre_jump   + '0011',
+}
+labels = {}
+variables = {}
+addressing_mode_opcodes = {
+    'reg_direct':              '000',
+    'reg_indirect':            '001',
+    'auto_increment':          '010',
+    'auto_increment_indirect': '011',
+    'auto_decrement':          '100',
+    'auto_decrement_indirect': '101',
+    'indexed':                 '110',
+    'indexed_indirect':        '111',
+}
+register_opcodes = {
+    'R0': '000',
+    'R1': '001',
+    'R2': '010',
+    'R3': '011',
+    'R4': '100',
+    'R5': '101',
+    'R6': '110',
+    'R7': '111',
+}
+two_op_instructions = [
+    'MOV',
+    'ADD',
+    'ADC',
+    'SUB',
+    'SBC',
+    'AND',
+    'OR',
+    'XOR',
+    'CMP',
+]
+one_op_instructions = [
+    'INC',
+    'DEC',
+    'CLR',
+    'INV',
+    'LSR',
+    'ROR',
+    'ASR',
+    'LSL',
+    'ROL',
+]
+branch_instructions = [
+    'BR',
+    'BEQ',
+    'BNE',
+    'BLO',
+    'BLS',
+    'BHI',
+    'BHS',
+]
+no_op_instructions = ['HLT', 'NOP', 'RESET']
+jsr_instructions = ['JSR', 'RTS', 'INTERRUPT', 'IRET']
+
+
+def get_addressing_mode(op):
+    tempBitString = ['', '']
+    register = re.search('R[0-7]', op)
+    if register:
+        register = register.group(0)
+
+    if re.match('^R[0-7]$', op):
+        tempBitString[0] += addressing_mode_opcodes['reg_direct'] \
+            + register_opcodes[register]
+
+    elif re.match(r"^\(([^)]+)\)\+$", op):
+        tempBitString[0] += addressing_mode_opcodes['auto_increment'] \
+            + register_opcodes[register]
+
+    elif re.match(r"^\-\(([^)]+)\)$", op):
+        tempBitString[0] += addressing_mode_opcodes['auto_decrement'] \
+            + register_opcodes[register]
+
+    elif re.match(r"^\d+\(([^)]+)\)$", op):
+        tempBitString[0] += addressing_mode_opcodes['indexed'] \
+            + register_opcodes[register]
+
+    elif re.match(r"^\@R[0-7]$", op):
+        tempBitString[0] += addressing_mode_opcodes['reg_indirect'] \
+            + register_opcodes[register]
+
+    elif re.match(r"^\@\(([^)]+)\)\+$", op):
+        tempBitString[0] += \
+            addressing_mode_opcodes['auto_increment_indirect'] \
+            + register_opcodes[register]
+
+    elif re.match(r"^\@\-\(([^)]+)\)$", op):
+        tempBitString[0] += \
+            addressing_mode_opcodes['auto_decrement_indirect'] \
+            + register_opcodes[register]
+
+    elif re.match(r"^\@\d+\(([^)]+)\)$", op):
+        tempBitString[0] += addressing_mode_opcodes['indexed_indirect'] \
+            + register_opcodes[register]
+
+    elif re.match(r"^\#\d+$", op):
+        value = int(re.search(r"\d+", op).group(0))
+        tempBitString[0] += addressing_mode_opcodes['auto_increment'] \
+            + register_opcodes['R7']
+        tempBitString[1] = f"{value:016b}"
+
+    elif re.match(r'^[A-Z]+$', op):
+        variable = re.search('[A-Z]', op).group(0)
+
+        if variable in variables:
+            address = variables[variable]['address']
+            tempBitString[0] += addressing_mode_opcodes['indexed'] \
+                + register_opcodes['R7']
+            tempBitString[1] += f"{address:016b}"
+
+        else:
+            address = '[' + variable + ']'
+            tempBitString[0] += addressing_mode_opcodes['indexed'] \
+                + register_opcodes['R7']
+            tempBitString[1] = address
+    else:
+        print('Error in parsing addressing mode')
+        sys.exit(1)
+
+    return tempBitString
+
+
+def handle_two_op_instruction(op1, op2):
+    firstOpBits = get_addressing_mode(op1)
+    secondOpBits = get_addressing_mode(op2)
+    operandsBitString = firstOpBits[0] + secondOpBits[0]
+    returnedBitArr = [firstOpBits[0] + secondOpBits[0], firstOpBits[1], secondOpBits[1]]
+    return returnedBitArr
+
+
+def handle_one_op_instruction(op):
+    opBits = get_addressing_mode(op)
+    returnedBitArr = [opBits[0] + '0' * (8 - len(opBits[0])), opBits[1]]
+    return returnedBitArr
+
+def build_lookup_table(filePath):
+  table = {}
+  with open(filePath, 'r') as f:
+    keyValueList = [x for x in f.read().splitlines() if x]
+    for kv in keyValueList:
+      string, code = kv.split()
+      table[string] = code
+
+  return table
+
+
+def define_variable(line, variables, Memory, curr_addr):
+  variable = line.split(' ')
+  variable = list(filter(lambda x: bool(x), variable))
+  variables[variable[1]] = {
+    'address': curr_addr,
+    'values': list(map(int, variable[2].split(',')))
+  }
+
+  for value in variables[variable[1]]['values']:
+    Memory[curr_addr] = f"{int(value) & 0xFFFF:016b}"
+    curr_addr += 1
+
+  return curr_addr
+
+
+def define_label(line, labels, curr_addr):
+  label = line.split(':')[0].upper()
+  labels[label] = curr_addr
+
+
+def sanitize_line(line):
+  line = re.split(';', line)[0]  # removing comments from each line
+  instructionString = re.split(':', line)[-1].strip().upper()  # removing labels
+  instruction = re.split(r",|\s", instructionString)
+  instruction = list(filter(lambda x: bool(x), instruction))
+  return instruction
+
+
+def sanitize_label(key, value, labels, Memory):
+  if re.search(r"\{\w+\}", value):
+      label = re.search(r"\{(\w+)\}", value).group(1)
+
+      offset = labels[label] - (key + 1)
+      if not -128 <= offset <= 127:
+          print('Offset out of range in line', index + 1)
+          sys.exit(1)
+
+      # Updating Memory Value
+      newValue = value.replace('{' + label + '}', '000' + f"{offset & 0xFF:08b}")
+      Memory[key] = newValue
+
+
+def sanitize_variable(key, value, variables, Memory):
+  if re.search(r"\[\w+\]", value):
+      var = re.search(r"\[(\w+)\]", value).group(1)
+      print(var)
+      Memory[key] = f"{variables[var]['address']:016b}"
+
+
+def process_two_op(instruction, bitString, Memory, curr_addr):
+  two_op_result = handle_two_op_instruction(instruction[1], instruction[2])
+
+  # Constructing Memory
+  Memory[curr_addr] = bitString + two_op_result[0]  # instruction in memory
+
+  if two_op_result[1]:
+    curr_addr += 1
+    Memory[curr_addr] = two_op_result[1]
+
+  if two_op_result[2]:
+    curr_addr += 1
+    Memory[curr_addr] = two_op_result[2]
+
+  return curr_addr
+
+
+def process_one_op(instruction, bitString, Memory, curr_addr):
+  one_op_result = handle_one_op_instruction(instruction[1])
+
+  # Constructing Memory
+  Memory[curr_addr] = bitString + one_op_result[0]
+
+  if one_op_result[1]:
+      curr_addr += 1
+      Memory[curr_addr] = one_op_result[1]
+
+  return curr_addr
+
+def process_branch(instruction, bitString, Memory, curr_addr):
+  if len(instruction) > 2:
+      print('Branch instruction expects a label only, in line', index + 1)
+      sys.exit(1)
+
+  label = instruction[1]
+
+  if label not in labels:
+      # Constructing Memory
+      Memory[curr_addr] = bitString + '{' + label + '}'
+
+  else:
+      offset = labels[label] - (curr_addr + 1)
+
+      if not -128 <= offset <= 127:
+          print('Offset out of range in line', index + 1)
+          sys.exit(1)
+
+      # Constructing Memory
+      Memory[curr_addr] = bitString + '000' + f"{offset & 0xFF:08b}"
+
+  return curr_addr
+
+
+def process_no_op(instruction, bitString, Memory, curr_addr):
+  if len(instruction) > 1:
+      print('no-operand instruction', instruction[0],
+            'expects no operands, in line', index + 1)
+      sys.exit(1)
+
+  # Constructing Memory
+  Memory[curr_addr] = bitString + '0' * (16 - len(bitString))
+
+  return curr_addr
+
+
+def process_jsr(instruction, bitString, Memory, curr_addr):
+  if instruction[0] == 'JSR':
+      if len(instruction) > 2:
+          print('JSR instruction expects a label only, in line', index + 1)
+          sys.exit(1)
+
+      Memory[curr_addr] = bitString + '0' * (16 - len(bitString))
+      curr_addr += 1
+
+      label = instruction[1]
+      if label in labels:
+          Memory[curr_addr] = labels[label]
+      else:
+          Memory[curr_addr] = '{' + label + '}'
+  else:
+      # Constructing Memory
+      Memory[curr_addr] = bitString + '0' * (16 - len(bitString))
+
+  return curr_addr
+
+def write_memory_to_file(filePath):
+  with open(filePath, 'w+b') as f:
+    for i in sorted(Memory):
+      word = int(Memory[i], 2).to_bytes(2, byteorder='big')
+      f.write(word)
