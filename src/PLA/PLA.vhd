4d4c3b576763c0614d5263fc5ad7df7fbe21227d
Youssef Walid <yousefwalid98@hotmail.com>
2021-01-11 19:41:53 +0200

    Added branch instructions in PLA

--- src/PLA/PLA.vhd
+++ src/PLA/PLA.vhd
@@ -12,12 +12,15 @@ end entity PLA;
 architecture default of PLA is
 	signal partAOutput, partBOutput, partCOutput, partDOutput : std_logic_vector(n-1 downto 0);
 	signal partACarry, partCCarry, partDCarry : std_logic;	
-
+	signal status_N, status_Z, status_V, status_C : std_logic;
 begin
+	status_N <= statusRegister(0);
+	status_Z <= statusRegister(1);
+	status_V <= statusRegister(2);
+	status_C <= statusRegister(3);
 	PROCESS (IR)
-
 	BEGIN
-		IF IR(n-1 DOWNTO n-4) = "1001" && (signed(controlStepCounter) = 3) THEN
+		IF IR(n-1 DOWNTO n-4) = "1001" AND (signed(controlStepCounter) = 3) THEN
 			-- one op instruction
 			
 			-- destination fetching
@@ -41,21 +44,67 @@ begin
 
 		ELSIF  IR(n-1 DOWNTO 14) = "11" THEN
 			-- branch instruction
-			IF IR(n-5 DOWNTO n-7) = "000" THEN
-				-- BR instruction
-			ELSIF IR(n-5 DOWNTO n-7) = "001" THEN
-				-- BEQ instruction
-			ELSIF IR(n-5 DOWNTO n-7) = "010" THEN
-				-- BNE instruction
-			ELSIF IR(n-5 DOWNTO n-7) = "011" THEN
-				-- BLO instruction
-			ELSIF IR(n-5 DOWNTO n-7) = "100" THEN
-				-- BLS instruction
-			ELSIF IR(n-5 DOWNTO n-7) = "101" THEN
-				-- BHI instruction
-			ELSIF IR(n-5 DOWNTO n-7) = "110" THEN
-				-- BHS instruction
+
+			IF (signed(controlStepCounter) = 3) THEN -- go to corresponding branch instruction
+				IF IR(n-5 DOWNTO n-7) = "000" THEN
+					-- BR instruction
+					load <= -- row 92 (Branch Offset)
+				ELSIF IR(n-5 DOWNTO n-7) = "001" THEN
+					-- BEQ instruction
+					IF Z = '1' THEN
+						load <= -- row 92 (Branch Offset)
+					ELSE
+						load <= (OTHERS => '0'); -- END
+					END
+				ELSIF IR(n-5 DOWNTO n-7) = "010" THEN
+					-- BNE instruction
+					IF Z = '0' THEN
+						load <= -- row 92 (Branch Offset)
+					ELSE
+						load <= (OTHERS => '0'); -- END
+					END
+				ELSIF IR(n-5 DOWNTO n-7) = "011" THEN
+					-- BLO instruction
+					IF C = '1' THEN
+						load <= -- row 92 (Branch Offset)
+					ELSE
+						load <= (OTHERS => '0'); -- END
+					END
+				ELSIF IR(n-5 DOWNTO n-7) = "100" THEN
+					-- BLS instruction
+					IF C = 0 OR Z = 1 THEN
+						load <= -- row 92 (Branch Offset)
+					ELSE
+						load <= (OTHERS => '0'); -- END
+					END
+				ELSIF IR(n-5 DOWNTO n-7) = "101" THEN
+					-- BHI instruction
+					IF C = 1 THEN
+						load <= -- row 92 (Branch Offset)
+					ELSE
+						load <= (OTHERS => '0'); -- END
+					END
+				ELSIF IR(n-5 DOWNTO n-7) = "110" THEN
+					-- BHS instruction
+					IF C = 1 OR Z = 1 THEN
+						load <= -- row 92 (Branch Offset)
+					ELSE
+						load <= (OTHERS => '0'); -- END
+					END
+			END
 			
+			IF (signed(controlStepCounter) = 6) THEN -- row 94: µ-PC <= PLA(IR)$ [Double Operand]::ADD SRC, DST
+					load <= -- row 48 (ADD SRC, DST)
+			END
+
+			IF (signed(controlStepCounter) = 9) THEN -- row 50: µ-PC <= PLA(IR)$ [Move Z to PC]
+				load <= -- row 95 (MOV Z to PC)
+			END
+
+			IF (signed(controlStepCounter) = 11) THEN -- row 96: END
+				load <= (OTHERS => '0')-- row 95 (MOV Z to PC)
+			END
+
 		
 		ELSIF IR(n-1 DOWNTO 12) = "1010" THEN
 			-- no op instruction
