7e34c8d6bbbe60bb7e352a57292f3967b014dfb8
Abdelrahman Farid <abdelrahman.farid99@eng-st.cu.edu.eg>
2021-01-13 23:39:00 +0200

    Modified RAM to delay a few clock cycles before finishing operations

--- src/RAM/nmRam.vhd
+++ src/RAM/nmRam.vhd
@@ -8,10 +8,11 @@ ENTITY nmRam IS
     n: INTEGER := 32;
     m: INTEGER := 32
   );
-	PORT(
-    MFC: OUT std_logic;
+  PORT(
     clk: IN std_logic;
-    wr: IN std_logic;
+    MFC: OUT std_logic;
+    read: in std_logic;
+    write: in std_logic;
     address: IN std_logic_vector(integer(ceil(log2(real(n))))-1 DOWNTO 0);
     dataIn: IN std_logic_vector(m-1 DOWNTO 0);
     dataOut: OUT std_logic_vector(m-1 DOWNTO 0)
@@ -19,6 +20,8 @@ ENTITY nmRam IS
 END ENTITY;
 
 ARCHITECTURE main OF nmRam IS
+  constant DELAY: integer := 2;
+
   TYPE ram_type IS ARRAY(0 TO n-1) of std_logic_vector(m-1 DOWNTO 0);
   SIGNAL ram: ram_type := (
     -- initialize here 
@@ -30,17 +33,29 @@ ARCHITECTURE main OF nmRam IS
   );
 BEGIN
 
-PROCESS(clk) IS
+-- Delay write operation by two clock cycles
+PROCESS(clk)
+  variable v_delay: natural range 0 to DELAY := 0;
 BEGIN
-
   IF(rising_edge(clk)) THEN
-    IF(wr = '1') THEN
-      ram(to_integer(unsigned(address))) <= dataIn;
+    v_delay := v_delay + 1;
+    
+    if v_delay = DELAY then
+      v_delay := 0;
+
+      if write = '1' then
+        ram(to_integer(unsigned(address))) <= dataIn;
+        dataOut <= dataIn;
+      elsif read = '1' then
+        dataOut <= ram(to_integer(unsigned(address)));
+      end if;
+
       MFC <= '1';
     else
       MFC <= '0';
-    END IF;
-  END IF;
-END PROCESS;
-  dataOut <= ram(to_integer(unsigned(address)));
-END ARCHITECTURE;
+    end if;
+
+  end if;
+
+end process;
+end architecture;
